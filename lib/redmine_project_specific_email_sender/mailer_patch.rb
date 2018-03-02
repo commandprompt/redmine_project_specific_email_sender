5dd7e55ca62e42903fbb229fc7704f57c8eec868
Eugene Dubinin <eugend@commandprompt.com>
2018-03-02 15:20:46 +0200

    move mail method definition from module to class_eval to avoid including it into into inheritance chain and avoid conflicting with *super* calls

--- lib/redmine_project_specific_email_sender/mailer_patch.rb
+++ lib/redmine_project_specific_email_sender/mailer_patch.rb
@@ -3,6 +3,61 @@ module RedmineProjectSpecificEmailSender
     def self.included(base)
       base.send(:include, InstanceMethods)
       base.class_eval do
+
+        def mail(headers={}, &block)
+          headers.reverse_merge! 'X-Mailer' => 'Redmine',
+                  'X-Redmine-Host' => Setting.host_name,
+                  'X-Redmine-Site' => Setting.app_title,
+                  'X-Auto-Response-Suppress' => 'All',
+                  'Auto-Submitted' => 'auto-generated',
+                  'From' => mail_from,
+                  'List-Id' => "<#{Setting.mail_from.to_s.gsub('@', '.')}>"
+
+          # Replaces users with their email addresses
+          [:to, :cc, :bcc].each do |key|
+            if headers[key].present?
+              headers[key] = self.class.email_addresses(headers[key])
+            end
+          end
+          # Removes the author from the recipients and cc
+          # if the author does not want to receive notifications
+          # about what the author do
+          if @author && @author.logged? && @author.pref.no_self_notified
+            addresses = @author.mails
+            headers[:to] -= addresses if headers[:to].is_a?(Array)
+            headers[:cc] -= addresses if headers[:cc].is_a?(Array)
+          end
+
+          if @author && @author.logged?
+            redmine_headers 'Sender' => @author.login
+          end
+
+          # Blind carbon copy recipients
+          if Setting.bcc_recipients?
+            headers[:bcc] = [headers[:to], headers[:cc]].flatten.uniq.reject(&:blank?)
+            headers[:to] = nil
+            headers[:cc] = nil
+          end
+
+          if @message_id_object
+            headers[:message_id] = "<#{self.class.message_id_for(@message_id_object)}>"
+          end
+          if @references_objects
+            headers[:references] = @references_objects.collect {|o| "<#{self.class.references_for(o)}>"}.join(' ')
+          end
+
+          m = if block_given?
+            super headers, &block
+          else
+            super headers do |format|
+              format.text
+              format.html unless Setting.plain_text_mail?
+            end
+          end
+          set_language_if_valid @initial_language
+          m
+        end
+
         alias_method_chain :mail_from, :project_specific_email
         alias_method_chain :issue_add, :project_specific_email
         alias_method_chain :issue_edit, :project_specific_email
@@ -17,49 +72,6 @@ module RedmineProjectSpecificEmailSender
       def mail_from
         Setting.mail_from
       end
-
-      def mail(headers={})
-        headers.merge! 'X-Mailer' => 'Redmine',
-                'X-Redmine-Host' => Setting.host_name,
-                'X-Redmine-Site' => Setting.app_title,
-                'X-Auto-Response-Suppress' => 'OOF',
-                'Auto-Submitted' => 'auto-generated',
-                'From' => mail_from,
-                'List-Id' => "<#{mail_from.to_s.gsub('@', '.')}>"
-
-        # Removes the author from the recipients and cc
-        # if he doesn't want to receive notifications about what he does
-        if @author && @author.logged? && @author.pref.no_self_notified
-          headers[:to].delete(@author.mail) if headers[:to].is_a?(Array)
-          headers[:cc].delete(@author.mail) if headers[:cc].is_a?(Array)
-        end
-
-        if @author && @author.logged?
-          redmine_headers 'Sender' => @author.login
-        end
-
-        # Blind carbon copy recipients
-        if Setting.bcc_recipients?
-          headers[:bcc] = [headers[:to], headers[:cc]].flatten.uniq.reject(&:blank?)
-          headers[:to] = nil
-          headers[:cc] = nil
-        end
-
-        if @message_id_object
-          headers[:message_id] = "<#{self.class.message_id_for(@message_id_object)}>"
-        end
-        if @references_objects
-          headers[:references] = @references_objects.collect {|o| "<#{self.class.message_id_for(o)}>"}.join(' ')
-        end
-
-        super headers do |format|
-          format.text
-          format.html unless Setting.plain_text_mail?
-        end
-
-        set_language_if_valid @initial_language
-      end
-
 
       def mail_from_with_project_specific_email
         @project ? @project.email : mail_from_without_project_specific_email
